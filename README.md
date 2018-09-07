450d1185dd19a3c835c0e69583eca7a56f770081
ghiliweld <ghiliaweld@gmail.com>
2018-09-06 22:52:45 -0400

    Tweaked MimoStore and wrote README documentation

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# orbit-mimo
+# mimo-orbit
 Custom identity store on OrbitDB
+
+An append-only log with traversable history. Add new claims about your identity to your DB. It uses digital signatures to ensure that all data added to the DB has been approved by the owner of the identity.
+
+## Table of Contents
+
+- [Install](#install)
+- [Usage](#usage)
+- [API](#api)
+- [License](#license)
+
+## Install
+```
+npm install mimo-orbit
+```
+
+## Usage
+
+First, create an instance of OrbitDB and Web3:
+
+```javascript
+const IPFS = require('ipfs')
+const OrbitDB = require('orbit-db')
+const Web3 = require('web3')
+const MimoStore = require('mimo-orbit')
+
+const ipfs = new IPFS()
+const web3 = new Web3()
+
+// add custom type to orbitdb
+OrbitDB.addDatabaseType(MimoStore.type, MimoStore)
+
+// instantiate custom store
+const orbitdb = new OrbitDB(ipfs)
+const store = orbitdb.create(ensname, web3.eth.ens.registrar.owner(ensname), CustomStore.type)
+```
+
+Add a claim to it, if the signature of the data is a valid one then the data will be added successfully:
+
+```javascript
+store.add({ bio: 'I <3 Mimo' }, signature)
+  .then(() => {
+    const items = store.iterator().collect()
+    items.forEach((e) => console.log(e.bio))
+    // "I <3 Mimo"
+  })
+```
+
+Later, when the database contains data, load the history and query when ready:
+
+```javascript
+store.events.on('ready', () => {
+  const items = log.iterator().collect()
+  items.forEach((e) => console.log(e.bio))
+  // "I <3 Mimo"
+})
+```
+
+## API
+
+See [orbit-db's API Documenations](https://github.com/haadcode/orbit-db/blob/master/API.md#eventlogname) on eventlogs (the base class for MimoStore) for full details. The only difference between MimoStore and EventStore is its use of signatures for adding data.
+
+## License
+
+[MIT](LICENSE) © 2018 Ghilia Weldesselasie
--- src/MimoStore.js
+++ src/MimoStore.js
@@ -2,6 +2,15 @@ const EventStore = require('orbit-db-eventstore');
 const EthCrypto = require('eth-crypto');
 
 class MimoStore extends EventStore {
+
+  /**
+   * Instantiates a MimoStore
+   *
+   * @param     {IPFS}      ipfs        An IPFS instance
+   * @param     {String}    dbname      The name of your database (should be the same as your ENS name)
+   * @param     {String}    dbowner     The Ethereum account that owns this DB
+   * @return    {MimoStore}             self
+   */
   constructor(ipfs, id, dbname, dbowner, options) {
     if(!options) options = {};
     super(ipfs, id, dbname, options);
@@ -9,27 +18,62 @@ class MimoStore extends EventStore {
     this.owner = dbowner;
   }
 
+  /**
+   * Add data to the DB
+   *
+   * @param     {Object}    data         The data we want to add
+   * @param     {String}    sig          A valid signature of the data by the owner
+   */
   add(data, sig) {
     if (!didOwnerSign(data, sig)) throw new Error('Owner did not authorize this');
-    else super.add(data);
+    super.add(data);
   }
 
+  /**
+   * Checks if data has been signed by the owner
+   *
+   * @param     {Object}    data         The data we signed
+   * @param     {String}    sig          A signature of the data
+   * @returns   {Boolean}                Was the data signed by the owner?
+   */
   didOwnerSign(data, sig) {
     return this.recover(data, sig) == this.owner;
   }
 
+  /**
+   * Recovers the signer of the data
+   *
+   * @param     {Object}    data         The data we signed
+   * @param     {String}    sig          A signature of the data
+   * @returns   {String}                 The Ethereum address that signed the data
+   */
   recover(data, sig) {
     EthCrypto.recover(sig, EthCrypto.hash.keccak256(JSON.stringify(data)));
   }
 
+  /**
+   * Get the name of the database
+   *
+   * @returns   {String}                 The name of the database, same as ENS name
+   */
   get ensname() {
     return this.dbname;
   }
 
+  /**
+   * Get the owner of the database
+   *
+   * @returns   {String}                 The owner of the ENS name and DB (an Ethereum address)
+   */
   get owner() {
-    return this.owner
+    return this.owner;
   }
 
+  /**
+   * Get the type of the database
+   *
+   * @returns   {String}                 The type of the DB, returns 'mimo'
+   */
   static get type() {
     return 'mimo';
   }
